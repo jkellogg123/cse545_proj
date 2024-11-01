da88cf682fbb8f7137455f242e51814286414390
John Kellogg <jjkell07@louisville.edu>
2024-11-01 13:54:41 -0400

    data loading

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
 import numpy as np
+import os
 from solution import Solution
 
 
+def load_data(name):
+    path = "./data/" + name
+    with open(path, 'r') as file:
+        file.readline()
+        line = file.readline()
+        n = int(line.strip().split(' ')[0])
 
-def load_data(path):
-    pass
+        data = []
+        file.readline()
+        for i in range(n):
+            data.append(file.readline().strip().split(' '))
+        data = np.array(data, dtype=np.uint8)
+        
+        # the way the data is formatted is goofy, this "corrects" it
+        file.readline()
+        sort = []
+        for i in range(n):
+            sort = np.array(file.readline().strip().split(' '), dtype=np.uint8)
+            data[i] = data[i][sort - 1]
+        
+        return data
+
 
 def main():
-    n = 10
-    path = "69"
+    # file = "tai44_0.txt"
+    # data = Solution.data = load_data(file)
 
-    Solution.data = load_data(path)
-    lol = Solution(n)
-    print(lol.data)
-
+    for file in os.listdir("data"):
+        print(file)
+        print(load_data(file))
+        print()
+    
 
 if __name__ == "__main__":
     main()
--- solution.py
+++ solution.py
@@ -6,12 +6,12 @@ class Solution:
     Object to represent a single solution.
 
     Attributes:
-        schedule (2D np array):     schedule[i, j] is the jth job of the ith machine
-        starts (2D np array):       starts[i, j] is the starting time of the jth job of ith machine
+        schedule (2D np array):         schedule[i, j] is the jth job of the ith machine
+        starts (2D np array):           starts[i, j] is the starting time of the jth job of ith machine
 
-        data (static, 2D np array): data[i, j] is the time-length of the jth job on the ith machine
-        cross_rate (static, float):         0 <= rate <= 1, denotes frequency of crossover operation
-        mutate_rate (static, float):         0 <= rate <= 1, denotes frequency of mutate operation
+        data (static, 2D np array):     data[i, j] is the time-length of the jth job on the ith machine
+        cross_rate (static, float):     0 <= rate <= 1, denotes frequency of crossover operation
+        mutate_rate (static, float):    0 <= rate <= 1, denotes frequency of mutate operation
 
     """
 
@@ -20,9 +20,13 @@ class Solution:
     mutate_rate = 0.02
 
     def __init__(self, n: int, rand=True):
-        if not rand or rand:
-            self.schedule = np.full((n, n), -1)
-            self.starts = np.full((n, n), -1)
+        self.schedule = np.full((n, n), -1)
+        self.starts = np.full((n, n), -1)
+        if rand and not self.data is None:
+            # TODO
+            # Create random solution for initializing population of chromosomes for genetic algorithm
+            # Maybe doesn't need to be random, but need some way to populate our initial generation
+            pass
 
     def mutate(self):
         pass
